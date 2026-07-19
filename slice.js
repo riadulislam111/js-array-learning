b73b4aa23a251af76abbe3ebd72a097903707d37
riadulislam111 <riadulislam.bd.pabna@gmail.com>
2026-07-19 15:55:51 +0600

    added all files and updates sort and slice

---
+++ slice.js
@@ -0,0 +1,11 @@
+const numbers = [1,2,45,3,43,4,5,65,6,45];
+
+const slice = numbers.slice(2, 6);
+// console.log(slice);
+// console.log(numbers)
+
+const parts = numbers.splice(2, 2, 455,555,333);
+console.log(parts);
+console.log(numbers
+
+)
---
+++ sort.js
@@ -0,0 +1,7 @@
+const numbers = [2,3,4,9,5,6];
+numbers.sort();
+console.log(numbers);
+
+const fruits = ['mango', 'jackfruit', 'guyava', 'papaya','banana', 'apple'];
+fruits.sort();
+console.log(fruits)
